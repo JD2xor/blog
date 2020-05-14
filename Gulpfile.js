1c8c0e3c40470a4ebfecaa8e5ec5692141bcd4fb
JD2xor <flere210.0323@gmail.com>
2020-05-14 19:28:17 +0900

    Add gulp

---
+++ Gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    minify = require('gulp-minify'),
+    exec = require('gulp-exec'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    del = require('del');
+
+function pygments(cb) {
+    var options = {
+        continueOnError: false,
+        pipeStdout: false
+    };
+    cb();
+}
+
+function scss() {
+    return gulp.src('scss/style.scss')
+        .pipe(sass()
+            .on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('static/css/'));
+}
+
+function minifyScss() {
+    return gulp.src('scss/style.scss')
+        .pipe(sass({ outputStyle: 'compressed' })
+            .on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('static/css/'));
+}
+
+exports.default = scss;
+
+exports.dist = minifyScss;
+
+function watch(cb) {
+    gulp.watch('scss/**/*.scss', gulp.series(scss));
+    cb();
+}
+
+exports.watch = watch;
